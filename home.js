70a84c7e6b851b8c2a4cfe1f49280ad94448a3b3
jangala-laxman <74813943+jangala-laxman@users.noreply.github.com>
2022-04-25 19:46:48 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const fetch = require('node-fetch');
+const { isAnyArrayBuffer } = require('util/types');
+const app = express()
+
+// async function getres()
+//         {
+//             const response = await fetch('https://api.tvmaze.com/schedule')
+//             const data = await response.json()
+//             console.log(data)
+//         }
+//         getres()
+
+let sum = 0;
+const numbers = new Array()
+
+// document.getElementById("username").value 
+numbers.push(10)
+console.log(numbers)
+
+const server = app.listen(3000,()=>{
+})
